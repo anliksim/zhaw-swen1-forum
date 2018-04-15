eaaa3793c965725e268fa615e3de158b5d9f93cb
Simon Anliker <anliksim@users.noreply.github.com>
2018-04-15 15:56:57 +0200

    extend impl based on sequence diagram

--- src/ch/zhaw/swen1/forum/domain/Forum.java
+++ src/ch/zhaw/swen1/forum/domain/Forum.java
@@ -1,85 +1,106 @@
 package ch.zhaw.swen1.forum.domain;
 
- 
+
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Represents the whole forum with its list of topics and users
- *
  */
 public class Forum {
-	 
-	private List<User> users = new ArrayList<>();
-	private List<Topic> topics = new ArrayList<>();
 
-	 
+    private List<User> users = new ArrayList<>();
+    private List<Topic> topics = new ArrayList<>();
 
-	public List<User> getUsers() {
-		return users;
-	}
-	
-	public List<Topic> getTopics() {
-		return topics;
-	}
 
-	 
-	
-	/**
-	 * Creates and adds a new topic with the specified attributes. 
-	 */
-	public Topic addTopic(String name, String description){
-		if (name == null){
-			throw new RuntimeException("Name must be not null");
-		}
-		Topic existingTopic = getTopicForName(name);
-		if (existingTopic != null){
-			throw new RuntimeException("A topic with the name " + name + " already exists");
-		}
-		Topic topic = new Topic(name, description);
-		topics.add(topic);
-		return topic;
-	}
-	
-	/**
-	 * Returns the topic with the specified name or null if there is no such topic. 
-	 */
-	protected Topic getTopicForName(String name){
-		for(Topic topic : topics){
-			if (topic.getName().equals(name)){
-				return topic;
-			}
-		}
-		return null;
-	}
-	
-	/**
-	 * Creates and adds a new user with the specified attributes. 
-	 */
-	public User addUser(String name, byte[] passwordHash){
-		if (name == null){
-			throw new RuntimeException("Name must be not null");
-		}
-		User existingUser = getUserForName(name);
-		if (existingUser != null){
-			throw new RuntimeException("A topic with the name " + name + " already exists");
-		}
-		User user = new User(name, passwordHash);
-		users.add(user);
-		return user;
-	}
-	
-	protected User getUserForName(String name){
-		for(User user : users){
-			if (user.getName().equals(name)){
-				return user;
-			}
-		}
-		return null;
-	}
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public List<Topic> getTopics() {
+        return topics;
+    }
+
+
+    /**
+     * Creates and adds a new topic with the specified attributes.
+     */
+    public Topic addTopic(String name, String description) {
+        if (name == null) {
+            throw new RuntimeException("Name must be not null");
+        }
+        Topic existingTopic = getTopicForName(name);
+        if (existingTopic != null) {
+            throw new RuntimeException("A topic with the name " + name + " already exists");
+        }
+        Topic topic = new Topic(name, description);
+        topics.add(topic);
+        return topic;
+    }
+
+    /**
+     * Returns the topic with the specified name or null if there is no such topic.
+     */
+    protected Topic getTopicForName(String name) {
+        for (Topic topic : topics) {
+            if (topic.getName().equals(name)) {
+                return topic;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Creates and adds a new user with the specified attributes.
+     */
+    public User addUser(String name, byte[] passwordHash) {
+        if (name == null) {
+            throw new RuntimeException("Name must be not null");
+        }
+        User existingUser = getUserForName(name);
+        if (existingUser != null) {
+            throw new RuntimeException("A topic with the name " + name + " already exists");
+        }
+        User user = new User(name, passwordHash);
+        users.add(user);
+        return user;
+    }
+
+    protected User getUserForName(String name) {
+        for (User user : users) {
+            if (user.getName().equals(name)) {
+                return user;
+            }
+        }
+        return null;
+    }
 
     public int getNbrOfContributionsPerUser(String name) {
         User user = getUserForName(name);
         return user == null ? 0 : user.getNbrOfContributions();
+    }
+
+    /**
+     * Authenticates with the system.
+     *
+     * @param name         name of user
+     * @param passwordHash password hash of user
+     * @return the user if authenticated
+     * @throws IllegalStateException if not authenticated
+     */
+    public User authenticate(String name, byte[] passwordHash) {
+        User user = getUserForName(name);
+        if (!user.testPassword(passwordHash)) {
+            throw new IllegalStateException("Authentication failed. User not permitted.");
+        }
+        return user;
+    }
+
+    public void addUserContribution(String name, byte[] passwordHash, String topic, String discussion, String content) {
+        User user = authenticate(name, passwordHash);
+        getTopicForName(topic)
+                .addDiscussion(discussion)
+                .addContribution(new Contribution(content, user, Instant.now()));
     }
 }
--- test/ch/zhaw/swen1/forum/domain/ForumTest.java
+++ test/ch/zhaw/swen1/forum/domain/ForumTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 
 
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,84 +15,98 @@ import java.util.stream.Collectors;
 /**
  * Combined tests for forum domain logic. Because domain classes are quite simple,
  * only integration tests have been written.
-  *
  */
 public class ForumTest {
-	private Forum forum;
+    private Forum forum;
 
 
-	@Before
-	public void setUp() {
-		forum = new Forum();
-	}
+    @Before
+    public void setUp() {
+        forum = new Forum();
+    }
 
-	@Test
-	public void addTopicToForumAndAssertIt() {
-		forum.addTopic("Topic", "Description");
-		assertEquals(1, forum.getTopics().size());
-		Topic topic = forum.getTopics().get(0);
-		assertEquals("Topic", topic.getName());
-		assertEquals("Description", topic.getDescription());
-	}
+    @Test
+    public void addTopicToForumAndAssertIt() {
+        forum.addTopic("Topic", "Description");
+        assertEquals(1, forum.getTopics().size());
+        Topic topic = forum.getTopics().get(0);
+        assertEquals("Topic", topic.getName());
+        assertEquals("Description", topic.getDescription());
+    }
 
-	@Test(expected=RuntimeException.class)
-	public void addTopicWithDuplicateNameToForumAndAssertException() {
-		forum.addTopic("Topic", "Description");
-		assertEquals(1, forum.getTopics().size());
-		forum.addTopic("Topic", "Description");
-	}
+    @Test(expected = RuntimeException.class)
+    public void addTopicWithDuplicateNameToForumAndAssertException() {
+        forum.addTopic("Topic", "Description");
+        assertEquals(1, forum.getTopics().size());
+        forum.addTopic("Topic", "Description");
+    }
 
-	@Test
-	public void addDiscussionToTopicAndCountIt(){
-		Topic topic = forum.addTopic("Topic", "Description");
-		Discussion discussion = topic.addDiscussion("Discussion");
-		assertEquals(1, topic.getDiscussions().size());
-		assertEquals("Discussion", discussion.getName());
-	}
+    @Test
+    public void addDiscussionToTopicAndCountIt() {
+        Topic topic = forum.addTopic("Topic", "Description");
+        Discussion discussion = topic.addDiscussion("Discussion");
+        assertEquals(1, topic.getDiscussions().size());
+        assertEquals("Discussion", discussion.getName());
+    }
 
-	@Test(expected=RuntimeException.class)
-	public void addDiscussionToTopicWithDuplicateNameAndAssertException(){
-		Topic topic = forum.addTopic("Topic", "Description");
-		topic.addDiscussion("Discussion");
-		topic.addDiscussion("Discussion");
-	}
+    @Test(expected = RuntimeException.class)
+    public void addDiscussionToTopicWithDuplicateNameAndAssertException() {
+        Topic topic = forum.addTopic("Topic", "Description");
+        topic.addDiscussion("Discussion");
+        topic.addDiscussion("Discussion");
+    }
 
-	@Test
-	public void addContributionToDiscussionAndCountIt(){
-		// user, topic and discussion must be created first
-		User user = forum.addUser("User", new byte[]{0});
-		Topic topic = forum.addTopic("Topic", "Description");
-		Discussion discussion = topic.addDiscussion("Discussion");
+    @Test
+    public void addContributionToDiscussionAndCountIt() {
+        // user, topic and discussion must be created first
+        User user = forum.addUser("User", new byte[]{0});
+        Topic topic = forum.addTopic("Topic", "Description");
+        Discussion discussion = topic.addDiscussion("Discussion");
 
-		// add contribution to discussion
-		discussion.addContribution(new Contribution("Content", user, Instant.now()));
+        // add contribution to discussion
+        discussion.addContribution(new Contribution("Content", user, Instant.now()));
 
-		// assert newly added contribution
-		assertEquals(1, discussion.getContributions().size());
-		Contribution contribution = discussion.getContributions().get(0);
-		assertNotNull(contribution);
-		assertEquals("Content", contribution.getContent());
-		assertSame(user, contribution.getUser());
+        // assert newly added contribution
+        assertEquals(1, discussion.getContributions().size());
+        Contribution contribution = discussion.getContributions().get(0);
+        assertNotNull(contribution);
+        assertEquals("Content", contribution.getContent());
+        assertSame(user, contribution.getUser());
 
-		// assert total number of contributions in forum
-		int totalContributions = forum.getUsers().stream()
-				.map(User::getName)
-				.mapToInt(forum::getNbrOfContributionsPerUser)
-				.sum();
-		assertEquals(1, totalContributions);
-	}
+        // assert total number of contributions in forum
+        int totalContributions = forum.getUsers().stream()
+                .map(User::getName)
+                .mapToInt(forum::getNbrOfContributionsPerUser)
+                .sum();
+        assertEquals(1, totalContributions);
+    }
 
-	@Test
-	public void addUserContributionAndAssertIt(){
-		User user = forum.addUser("User", new byte[]{0});
-		Contribution contribution = new Contribution("Content ", user, Instant.now());
-		assertEquals(contribution, user.getContributions().get(0));
-		assertEquals(1, user.getNbrOfContributions());
-	}
+    @Test(expected = IllegalStateException.class)
+    public void addUserContributionWhenNotAuthenticated() {
+        User user = forum.addUser("User", "secret".getBytes());
+        forum.addUserContribution(user.getName(), new byte[]{0}, "any", "any", "any");
+    }
 
-	@Test
-	public void getUserForNotExistingNameAndAssertIt(){
-		assertNull(forum.getUserForName("NotExisting"));
-	}
+    @Test
+    public void addUserContributionAndAssertIt() {
+        byte[] hash = "secret".getBytes();
+        User user = forum.addUser("User", hash);
+        Topic topic = forum.addTopic("Topic", "Description");
+
+        // add contribution
+        forum.addUserContribution(user.getName(), hash, topic.getName(), "Discussion", "Content");
+
+        // assert it
+        assertEquals(1, topic.getDiscussions().size());
+        assertNotNull(topic.getDiscussionForName("Discussion"));
+        assertEquals(1, user.getNbrOfContributions());
+        Contribution contribution = user.getContributions().get(0);
+        assertEquals("Content", contribution.getContent());
+    }
+
+    @Test
+    public void getUserForNotExistingNameAndAssertIt() {
+        assertNull(forum.getUserForName("NotExisting"));
+    }
 
 }
